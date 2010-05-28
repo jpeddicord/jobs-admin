c2d20e346afafbc700b1b2eb2512ef88ff6a9329
Jacob Peddicord <jpeddicord@ubuntu.com>
2010-05-28 01:00:06 -0400

    Add loading of jobs/instances over dbus, displayed in treeview

--- JobsAdmin/__init__.py
+++ JobsAdmin/__init__.py
@@ -1,12 +1,14 @@
 
 import gtk
+from remote import RemoteJobService
 
 
 class JobsAdminUI:
 
-    def __init__(self, ui_file):
+    def __init__(self):
+        self.jobservice = RemoteJobService()
         self.builder = gtk.Builder()
-        self.builder.add_from_file(ui_file)
+        self.builder.add_from_file('jobs-admin.ui')
         
         objects = [
             'win_main',
@@ -23,10 +25,13 @@ class JobsAdminUI:
         for obj in objects:
             self.__dict__[obj] = self.builder.get_object(obj)
         
+        self.win_main.connect('destroy', gtk.main_quit)
         self.btn_close.connect('clicked', gtk.main_quit)
     
     def load_jobs(self):
-        
+        for job in self.jobservice.get_all_jobs():
+            self.lst_jobs.append((job.name, job.running))
+        self.lst_jobs.set_sort_column_id(0, gtk.SORT_ASCENDING)
     
     def run_settings_dialog(self, jobname):
         pass
--- JobsAdmin/remote.py
+++ JobsAdmin/remote.py
@@ -2,9 +2,10 @@
 from dbus import SystemBus, Interface
 
 
-class RemoteJobService(self):
+class RemoteJobService:
     
     def __init__(self):
+        self.jobs = []
         self.bus = SystemBus()
         self.jobservice = Interface(
             self.bus.get_object('com.ubuntu.JobService', '/com/ubuntu/JobService'),
@@ -12,13 +13,18 @@ class RemoteJobService(self):
         )
     
     def get_all_jobs(self):
-        #TODO: return a list of RemoteJob objects
-        self.jobservice.GetAllJobs()
+        for job, running in self.jobservice.GetAllJobs():
+            self.jobs.append(RemoteJob(job, running))
+        return self.jobs
     
 
 class RemoteJob:
     
-    def __init__(self):
-        pass
+    def __init__(self, name, running):
+        self.name = name
+        self.running = running
+    
+    def __unicode__(self):
+        return self.name
     
     
