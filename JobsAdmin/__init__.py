cc2ebedd86d3e7845a8072ba95f45ca64eea7031
Jacob Peddicord <jpeddicord@ubuntu.com>
2010-05-31 15:45:29 -0400

    Make running jobs bold in the list

--- JobsAdmin/__init__.py
+++ JobsAdmin/__init__.py
@@ -34,16 +34,18 @@ class JobsAdminUI:
     
     def load_jobs(self):
         for jobname, job in self.jobservice.get_all_jobs().iteritems():
-            self.lst_jobs.append((jobname, job.running))
+            self.lst_jobs.append((jobname, 700 if job.running else 400))
         self.lst_jobs.set_sort_column_id(0, gtk.SORT_ASCENDING)
         # put the cursor on something to trigger show_job_details
         self.tv_jobs.set_cursor(0)
     
     def show_job_details(self, treeview):
         model, treeiter = self.tv_jobs_sel.get_selected()
+        # we must have a selection at this point
         assert treeiter
         jobname = self.lst_jobs.get_value(treeiter, 0)
         job = self.jobservice.jobs[jobname]
+        # update some ui elements
         self.lbl_jobdesc.props.label = "<b>%s</b> - <i>%s</i>" % (
                 jobname, job.description)
         if job.running:
