029215bc420df8ec53226139f0ad4ecc8c451b05
Jacob Peddicord <jpeddicord@ubuntu.com>
2010-06-21 20:47:12 -0400

    setup: add bzr revision to the end of the version

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 from distutils.core import setup
+from subprocess import Popen, PIPE
 
 
 setup_info = dict(
     name='jobs-admin',
-    version='0.1',
+    version='0.1~bzr',
     description='jobs-admin',
     author='Jacob Peddicord',
     author_email='jpeddicord@ubuntu.com',
@@ -17,6 +18,12 @@ setup_info = dict(
         ('share/jobs-admin', ['jobs-admin.ui']),
     ]
 )
+
+# get the bzr revision if applicable
+if 'bzr' in setup_info['version']:
+    try:
+        setup_info['version'] += Popen(['bzr', 'revno'],stdout=PIPE).communicate()[0].strip()
+    except: pass
 
 setup(**setup_info)
 
