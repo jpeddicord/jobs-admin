e167f2d0e4446ef11b3928944796c709df4fc0a7
Jacob Peddicord <jpeddicord@ubuntu.com>
2010-07-01 18:57:09 -0400

    Add packagekit extra (not finished). Renamed apport => apportreport and packagekit => pkit, otherwise these collide with system packages

--- JobsAdmin/extras/__init__.py
+++ JobsAdmin/extras/__init__.py
@@ -11,12 +11,12 @@ class AllExtras(ExtraBase):
     def __init__(self, ui):
         self.ui = ui
         self.extras = []
-        for extra in ('apport', 'packagekit'):
+        for extra in ('apportreport', 'pkit'):
             try:
                 mod = __import__('JobsAdmin.extras.' + extra, fromlist=['Extra'])
                 self.extras.append(mod.Extra(self.ui))
-            except:
-                pass
+            except Exception, e:
+                print e
     
     def update_ui(self):
         for extra in self.extras:
