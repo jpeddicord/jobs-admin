015c71667f1ec6057e0754908f582d0b3b1149d4
Jacob Peddicord <jpeddicord@ubuntu.com>
2010-07-26 11:28:02 -0400

    Support the jobservice change for settings as lists.

--- JobsAdmin/settings.py
+++ JobsAdmin/settings.py
@@ -22,55 +22,56 @@ class SettingsDialog(gtk.Dialog):
         
         row = 0
         # add the settings fields
-        for name, details in self.settings.iteritems():
+        for name, stype, desc, val, vals, constrs in self.settings:
             # display varies by type
-            if details[0] == 'bool':
-                widget = gtk.CheckButton(details[1])
-                widget.props.active = (details[2] == 'true')
+            if stype == 'bool':
+                widget = gtk.CheckButton(desc)
+                widget.props.active = (val == 'true')
             
-            elif details[0] == 'int':
-                imin = details[4]['min'] if 'min' in details[4] else -65535
-                imax = details[4]['max'] if 'max' in details[4] else 65535
-                adj = gtk.Adjustment(int(details[2]),
+            elif stype == 'int':
+                imin = constrs['min'] if 'min' in constrs else -65535
+                imax = constrs['max'] if 'max' in constrs else 65535
+                adj = gtk.Adjustment(int(val),
                         float(imin), float(imax), 1)
                 widget = gtk.SpinButton(adj)
             
-            elif details[0] == 'float':
-                imin = details[4]['min'] if 'min' in details[4] else -65535
-                imax = details[4]['max'] if 'max' in details[4] else 65535
-                adj = gtk.Adjustment(float(details[2]),
+            elif stype == 'float':
+                imin = constrs['min'] if 'min' in constrs else -65535
+                imax = constrs['max'] if 'max' in constrs else 65535
+                adj = gtk.Adjustment(float(val),
                         float(imin), float(imax), 0.1)
                 widget = gtk.SpinButton(adj, digits=1)
             
-            elif details[0] == 'str':
-                widget = gtk.Entry()
-                widget.props.text = details[2]
-            
-            elif details[0] == 'choice':
+            elif stype == 'choice':
                 lst = gtk.ListStore(str, str)
                 widget = gtk.ComboBox(lst)
                 cell = gtk.CellRendererText()
                 widget.pack_start(cell)
                 widget.add_attribute(cell, 'text', 1)
                 index = 0
-                for vname, vdesc in details[3]:
+                for vname, vdesc in vals:
                     lst.append((vname, vdesc))
-                    if vname == details[2]:
+                    if vname == val:
                         widget.props.active = index
                     index += 1
             
-            elif details[0] == 'file':
+            elif stype == 'file':
                 widget = gtk.FileChooserButton("Choose a file")
             
-            elif details[0] == 'dir':
+            elif stype == 'dir':
                 widget = gtk.FileChooserButton("Choose a folder")
             
+            # use a string for 'str' and all other unknowns
+            else:
+                widget = gtk.Entry()
+                widget.props.text = val
+            
             # checkboxes already have labels
-            if details[0] == 'bool':
+            if stype == 'bool':
                 self.table.attach(widget, 0, 2, row, row + 1)
             # but the others don't; let's add them
             else:
-                lbl = gtk.Label("{0}:".format(details[1]))
+                lbl = gtk.Label("{0}:".format(desc))
                 lbl.props.xalign = 0
                 self.table.attach(lbl, 0, 1, row, row + 1)
                 self.table.attach(widget, 1, 2, row, row + 1)
@@ -85,22 +86,23 @@ class SettingsDialog(gtk.Dialog):
         if response != gtk.RESPONSE_ACCEPT:
             return
         newsettings = {}
-        for name, details in self.settings.iteritems():
-            widget = self.widgets[name]
+        for setting in self.settings:
+            widget = self.widgets[setting[0]]
             # grab the value based on type
-            if details[0] == 'bool':
+            if setting[1] == 'bool':
                 value = 'true' if widget.props.active else 'false'
-            elif details[0] == 'int':
+            elif setting[1] == 'int':
                 value = str(int(widget.props.value))
-            elif details[0] == 'float':
+            elif setting[1] == 'float':
                 value = str(widget.props.value)
-            elif details[0] == 'str':
+            elif setting[1] == 'str':
                 value = widget.props.text
-            elif details[0] == 'choice':
+            elif setting[1] == 'choice':
                 value = widget.get_model()[widget.props.active][0]
-            elif details[0] == 'file' or details[0] == 'dir':
+            elif setting[1] == 'file' or setting[1] == 'dir':
                 value = "unknown" #TODO
             # only send it if changed
-            if value != details[2]:
-                newsettings[name] = value
+            if value != setting[3]:
+                newsettings[setting[0]] = value
         self.job.set_settings(newsettings)
+
