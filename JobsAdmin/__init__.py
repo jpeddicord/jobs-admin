09e814b59ccd95ca3a88ef0cb2f456beaec820b6
Jacob Peddicord <jpeddicord@ubuntu.com>
2010-07-29 18:02:17 -0400

    Add copyright headers.

--- JobsAdmin/__init__.py
+++ JobsAdmin/__init__.py
@@ -1,3 +1,18 @@
+# This file is part of jobs-admin.
+# Copyright 2010 Jacob Peddicord <jpeddicord@ubuntu.com>
+#
+# jobs-admin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# jobs-admin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with jobs-admin.  If not, see <http://www.gnu.org/licenses/>.
 
 import gtk
 from gobject import idle_add
